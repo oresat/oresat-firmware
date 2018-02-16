baee852f185c1ec008e2a5392096d2091fa10a7e
Miles Simpson <miles.a.simpson@gmail.com>
2018-02-16 12:19:22 -0800

    Initialized app template directory

---
+++ src/app_template/source/example.c
@@ -0,0 +1,5 @@
+/*
+ * Example app-specific implementation file
+ */
+
+#include "example.h"
---
+++ src/app_template/source/example.h
@@ -0,0 +1,9 @@
+/*
+ * Example app-specific header file
+ */
+
+#ifndef _EXAMPLE_H_
+#define _EXAMPLE_H_
+
+
+#endif
