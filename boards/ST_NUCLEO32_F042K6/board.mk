d2122c2775a6c8b1f7c0046ee8a5631ac359cecd
Miles Simpson <miles.a.simpson@gmail.com>
2018-02-16 12:14:58 -0800

    Updated board encodings to UNIX

--- boards/ST_NUCLEO32_F042K6/board.mk
+++ boards/ST_NUCLEO32_F042K6/board.mk
@@ -1,8 +1,8 @@
 # List of all the board related files.
-BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO32_F042K6/board.c
+BOARDSRC = ./board.c
 
 # Required include directories
-BOARDINC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO32_F042K6
+BOARDINC = .
 
 # Shared variables
 ALLCSRC += $(BOARDSRC)
