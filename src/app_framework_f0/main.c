0091c4ecea9f204017cbc93979c87ff927b2ee86
Miles Simpson <miles.a.simpson@gmail.com>
2018-02-23 14:56:32 -0800

    Fixed order of makefile includes

---
+++ src/app_framework_f0/main.c
@@ -0,0 +1,91 @@
+/*! \file main.c
+ *  app_template
+ *
+ */
+
+/*
+    ChibiOS - Copyright (C) 2006..2018 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/*!
+ * \defgroup main app_template main
+ *
+ * @{
+ */
+
+/*
+ * ChibiOS header files
+ */
+#include "ch.h"
+#include "hal.h"
+
+/*
+ * Project header files
+ */
+#include "can.h"
+
+/*
+ * Serial configuration
+ */
+static SerialConfig ser_cfg =
+{
+    115200,     //Baud rate
+    0,          //
+    0,          //
+    0,          //
+};
+
+static void app_init(void)
+{
+    // Start up debug output
+    sdStart(&SD2, &ser_cfg);
+
+    // Initialize CAN Subsystem
+    can_init();
+}
+
+static void main_app(void)
+{
+    // Start CAN threads
+    can_start();
+
+    /*
+     * Begin main loop
+     */
+    while (true)
+    {
+        chThdSleepMilliseconds(1000);
+    }
+}
+
+int main(void)
+{
+    /*
+     * System initializations.
+     * - HAL initialization, this also initializes the configured device drivers
+     *   and performs the board-specific initializations.
+     * - Kernel initialization, the main() function becomes a thread and the
+     *   RTOS is active.
+     */
+    halInit();
+    chSysInit();
+    app_init();
+
+    main_app();
+
+    return 0;
+}
+
+//! @}
