0ba4430fdb43a5600f4f4ec1a58f89c85d88f9af
Miles Simpson <miles.a.simpson@gmail.com>
2018-02-26 20:33:16 -0800

    Restructured into f0 and f4 directories

--- toolchain/openocd/gdboocd_ST_NUCLEO64_F446RE.cmd
+++ toolchain/openocd/gdboocd_ST_NUCLEO64_F446RE.cmd
@@ -1,3 +1,3 @@
-target remote | openocd -s ../../toolchain/openocd -f oocd_ST_NUCLEO64_F446RE.cfg -c "gdb_port pipe"
+target remote | openocd -s ../../../toolchain/openocd -f oocd_ST_NUCLEO64_F446RE.cfg -c "gdb_port pipe"
 monitor stm32f4x.cpu configure -rtos ChibiOS
 monitor reset halt
