7c1033d23a8eeccd544a8ffadaecaddc81219725
Miles Simpson <miles.a.simpson@gmail.com>
2018-03-08 15:52:58 -0800

    Initial commit of protocard demo

---
+++ src/f0/app_protodemo/source/thread1.c
@@ -0,0 +1,16 @@
+#include "thread1.h"
+
+// Example blinker thread
+THD_WORKING_AREA(waThread1, 64);
+THD_FUNCTION(Thread1, arg) {
+
+  (void)arg;
+  chRegSetThreadName("thread1");
+
+  while (!chThdShouldTerminateX()) {
+    palClearLine(LINE_LED_GREEN);
+    chThdSleepMilliseconds(500);
+    palSetLine(LINE_LED_GREEN);
+    chThdSleepMilliseconds(500);
+  }
+}
---
+++ src/f0/app_protodemo/source/thread1.h
@@ -0,0 +1,11 @@
+#ifndef _THREAD1_H_
+#define _THREAD1_H_
+
+#include "ch.h"
+#include "hal.h"
+
+// Example blinker thread prototypes
+extern THD_WORKING_AREA(waThread1, 64);
+extern THD_FUNCTION(Thread1, arg);
+
+#endif
