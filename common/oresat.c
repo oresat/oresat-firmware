d2694ef56ce7e8a106d9e8973313a9910834f7bd
Miles Simpson <miles.a.simpson@gmail.com>
2018-04-02 11:45:13 -0700

    Fixed error in assignment

--- common/oresat.c
+++ common/oresat.c
@@ -3,7 +3,7 @@
 void oresat_init(uint8_t node_id) {
     if (node_id == 0) {
         //TODO: Check if the user data bytes are set and set Node ID from that if so
-        node_id == 0x7F;
+        node_id = 0x7F;
     }
     //Initialize CAN Subsystem
     can_init(node_id, 500);
