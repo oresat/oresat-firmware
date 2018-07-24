143b3c2140447881f085ee9daf42687194ceda61
Miles Simpson <miles.a.simpson@gmail.com>
2018-07-23 19:23:54 -0700

    Fixed broken F4 CAN configuration

--- common/can_hw.c
+++ common/can_hw.c
@@ -6,11 +6,10 @@
 
 /*
  * CAN Register configuration
- * See section 22.7.7 on the STM32 reference manual.
  * Timing calculator:
  * http://www.bittiming.can-wiki.info/
- * Default: 500Kbps based on 48MHz clock
  */
+#ifdef STM32F0xx_MCUCONF
 const CANConfig cancfg = {
     // MCR (Master Control Register)
     CAN_MCR_ABOM      |     //Automatic Bus-Off Management
@@ -24,6 +23,23 @@ const CANConfig cancfg = {
     CAN_BTR_TS2(1)    |     //Time Segment 2
     CAN_BTR_BRP(5)          //Bit Rate Prescaler
 };
+#endif
+
+#ifdef STM32F4xx_MCUCONF
+const CANConfig cancfg = {
+    // MCR (Master Control Register)
+    CAN_MCR_ABOM      |     //Automatic Bus-Off Management
+    CAN_MCR_AWUM      |     //Automatic Wakeup Mode
+    CAN_MCR_TXFP      ,     //Transmit FIFO Priority
+    // BTR (Bit Timing Register)
+    // Note: Convert to zero based values here when using the calculator
+    // CAN_BTR_LBKM     |     //Loopback Mode (Debug)
+    CAN_BTR_SJW(0)    |     //Synchronization Jump Width
+    CAN_BTR_TS1(14)   |     //Time Segment 1
+    CAN_BTR_TS2(1)    |     //Time Segment 2
+    CAN_BTR_BRP(4)          //Bit Rate Prescaler
+};
+#endif
 
 uint8_t can_hw_init(void) {
     /*
