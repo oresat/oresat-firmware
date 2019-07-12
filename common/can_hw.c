51fa4601ea7a0d5bb462601f6164de8e177f6b8a
Miles Simpson <miles.a.simpson@gmail.com>
2019-07-12 12:03:08 -0700

    Added initial L452 code

--- common/can_hw.c
+++ common/can_hw.c
@@ -41,6 +41,22 @@ const CANConfig cancfg = {
 };
 #endif
 
+#ifdef STM32L4xx_MCUCONF
+const CANConfig cancfg = {
+    // MCR (Master Control Register)
+    CAN_MCR_ABOM      |     //Automatic Bus-Off Management
+    CAN_MCR_AWUM      |     //Automatic Wakeup Mode
+    CAN_MCR_TXFP      ,     //Transmit FIFO Priority
+    // BTR (Bit Timing Register)
+    // Note: Convert to zero based values here when using the calculator
+    // CAN_BTR_LBKM     |     //Loopback Mode (Debug)
+    CAN_BTR_SJW(0)    |     //Synchronization Jump Width
+    CAN_BTR_TS1(14)   |     //Time Segment 1
+    CAN_BTR_TS2(1)    |     //Time Segment 2
+    CAN_BTR_BRP(4)          //Bit Rate Prescaler
+};
+#endif
+
 uint8_t can_hw_init(void) {
     /*
      * Activates CAN driver 1.
