d058f60fb1d4ce8d9bed84f24fc50c4ff80e44aa
Miles Simpson <miles.a.simpson@gmail.com>
2019-08-01 10:02:26 -0700

    Incorporated CANopenNode submodule

--- common/common.mk
+++ common/common.mk
@@ -1,14 +1,28 @@
-# Source files located under $(PROJ_SRC) are automatically added.
-rwildcard    = $(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
-PROJCSRC    := $(call rwildcard,$(PROJ_SRC),*.c)
-PROJCPPSRC  := $(call rwildcard,$(PROJ_SRC),*.cpp)
-PROJASMSRC  := $(call rwildcard,$(PROJ_SRC),*.s)
-PROJXASMSRC := $(call rwildcard,$(PROJ_SRC),*.S)
-PROJINC     := $(sort $(dir $(call rwildcard,$(PROJ_SRC),*)))
+CANOPEN_SRC   = $(PROJ_SRC)/CANopenNode
+CO_STACK      = $(CANOPEN_SRC)/stack
+
+PROJINC       = $(PROJ_SRC)/include             \
+                $(CANOPEN_SRC)                  \
+                $(CO_STACK)
+
+PROJSRC       = $(PROJ_SRC)/can_hw.c            \
+                $(PROJ_SRC)/can_threads.c       \
+                $(PROJ_SRC)/can.c               \
+                $(PROJ_SRC)/CO_driver.c         \
+                $(CO_STACK)/CO_SDO.c            \
+                $(CO_STACK)/CO_Emergency.c      \
+                $(CO_STACK)/CO_NMT_Heartbeat.c  \
+                $(CO_STACK)/CO_SYNC.c           \
+                $(CO_STACK)/CO_PDO.c            \
+                $(CO_STACK)/CO_HBconsumer.c     \
+                $(CO_STACK)/CO_SDOmaster.c      \
+                $(CO_STACK)/CO_LSSmaster.c      \
+                $(CO_STACK)/CO_LSSslave.c       \
+                $(CO_STACK)/CO_trace.c          \
+                $(CANOPEN_SRC)/CANopen.c        \
+                $(PROJ_SRC)/oresat.c
+
 
 # Shared variables.
-ALLCSRC     += $(PROJCSRC)
-ALLCPPSRC   += $(PROJCPPSRC)
-ALLASMSRC   += $(PROJASMSRC)
-ALLXASMSRC  += $(PROJXASMSRC)
-ALLINC      += $(PROJINC)
+ALLCSRC      += $(PROJSRC)
+ALLINC       += $(PROJINC)
