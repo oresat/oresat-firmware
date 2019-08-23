150ad5481241dd3190cc0866df006dce83638ff2
Miles Simpson <miles.a.simpson@gmail.com>
2019-08-22 17:37:33 -0700

    Attempting to get block transfer running

--- src/f4/app_nmt/source/nmt.c
+++ src/f4/app_nmt/source/nmt.c
@@ -6,6 +6,12 @@
 #include "chprintf.h"
 #include "shell.h"
 
+#ifndef BUF_SIZE
+#define BUF_SIZE 0x10000 /* 64k */
+#endif
+
+static uint8_t data[BUF_SIZE];
+
 void cmd_nmt(BaseSequentialStream *chp, int argc, char *argv[]) {
     uint8_t node_id = 0;
     if (argc < 2) {
@@ -31,24 +37,26 @@ void cmd_nmt(BaseSequentialStream *chp, int argc, char *argv[]) {
 }
 
 void cmd_sdo(BaseSequentialStream *chp, int argc, char *argv[]) {
-    uint8_t data[101] = {0};
     uint32_t data_len = 0;
     uint32_t abrt_code = 0;
     uint8_t node_id = 0;
     uint16_t index = 0;
     uint8_t subindex = 0;
+    bool block = false;
 
     if (argc < 4) {
-        chprintf(chp, "Usage: sdo read|write <NodeID> <index> <subindex> [write_value]\r\n");
+        chprintf(chp, "Usage: sdo read|write <NodeID> <index> <subindex> [blockmode]\r\n");
         return;
     }
 
     node_id = strtoul(argv[1], NULL, 0);
     index = strtoul(argv[2], NULL, 0);
     subindex = strtoul(argv[3], NULL, 0);
+    if (argc == 5)
+        block = !strcmp(argv[4], "true");
 
     if (!strcmp(argv[0], "read")) {
-        sdo_upload(CO->SDOclient[0], node_id, index, subindex, data, sizeof(data) - 1, &data_len, &abrt_code, 1000, false);
+        sdo_upload(CO->SDOclient[0], node_id, index, subindex, data, sizeof(data) - 1, &data_len, &abrt_code, 1000, block);
         data[data_len] = '\0';
         if (abrt_code == CO_SDO_AB_NONE) {
             chprintf(chp, "Received %u bytes of data:", data_len);
@@ -58,10 +66,10 @@ void cmd_sdo(BaseSequentialStream *chp, int argc, char *argv[]) {
         } else {
             chprintf(chp, "Received abort code: %x\r\n", abrt_code);
         }
-    } else if (!strcmp(argv[0], "write") && argc < 5) {
-        /*sdo_download(CO->SDOclient[0], node_id, index, subindex, data, strlen(data), &abrt_code, 1000, false);*/
+    } else if (!strcmp(argv[0], "write")) {
+        sdo_download(CO->SDOclient[0], node_id, index, subindex, data, data_len, &abrt_code, 1000, block);
     } else {
-        chprintf(chp, "Usage: sdo read|write <NodeID> <index> <subindex> <datatype> [write_value]\r\n");
+        chprintf(chp, "Usage: sdo read|write <NodeID> <index> <subindex> [block_mode]\r\n");
         return;
     }
 }
