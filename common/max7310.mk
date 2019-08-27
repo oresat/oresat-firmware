bfeaa1e29a37e12c19bf5a5528eea071f541d5fb
Miles Simpson <miles.a.simpson@gmail.com>
2019-08-27 14:04:22 -0700

    Initial commit of MAX7310 driver code

---
+++ common/max7310.mk
@@ -0,0 +1,9 @@
+# List of all the MAX7310 device files.
+MAX7310SRC := $(PROJ_SRC)/max7310.c
+
+# Required include directories
+MAX7310INC := $(PROJ_SRC)/include
+
+# Shared variables
+ALLCSRC += $(MAX7310SRC)
+ALLINC  += $(MAX7310INC)
