5fd665978dfba1771804e961fe71dd779b24e822
Miles Simpson <miles.a.simpson@gmail.com>
2019-08-27 16:20:07 -0700

    Initial commit of OPD API, WIP

---
+++ src/f4/app_nmt/source/opd.c
@@ -0,0 +1,55 @@
+#include "hal.h"
+#include "max7310.h"
+#include "opd.h"
+
+/* IO Pin Assignments */
+#define OPD_SCL             0U
+#define OPD_SDA             1U
+#define OPD_FAULT           2U
+#define OPD_EN              3U
+#define OPD_CB_RESET        4U
+#define OPD_BOOT0           5U
+#define OPD_PULLUP          6U
+#define OPD_LED             7U
+
+/* IO Pin Configurations */
+#define OPD_ODR_VAL        (MAX7310_PIN_ODR_HIGH(OPD_SCL)           |   \
+                            MAX7310_PIN_ODR_HIGH(OPD_SDA)           |   \
+                            MAX7310_PIN_ODR_LOW(OPD_FAULT)          |   \
+                            MAX7310_PIN_ODR_LOW(OPD_EN)             |   \
+                            MAX7310_PIN_ODR_LOW(OPD_CB_RESET)       |   \
+                            MAX7310_PIN_ODR_LOW(OPD_BOOT0)          |   \
+                            MAX7310_PIN_ODR_LOW(OPD_PULLUP)         |   \
+                            MAX7310_PIN_ODR_HIGH(OPD_LED))
+#define OPD_POL_VAL        (MAX7310_PIN_POL_INV(OPD_SCL)            |   \
+                            MAX7310_PIN_POL_INV(OPD_SDA)            |   \
+                            MAX7310_PIN_POL_INV(OPD_FAULT)          |   \
+                            MAX7310_PIN_POL_STD(OPD_EN)             |   \
+                            MAX7310_PIN_POL_STD(OPD_CB_RESET)       |   \
+                            MAX7310_PIN_POL_STD(OPD_BOOT0)          |   \
+                            MAX7310_PIN_POL_STD(OPD_PULLUP)         |   \
+                            MAX7310_PIN_POL_STD(OPD_LED))
+#define OPD_MODE_VAL       (MAX7310_PIN_MODE_OUTPUT(OPD_SCL)        |   \
+                            MAX7310_PIN_MODE_OUTPUT(OPD_SDA)        |   \
+                            MAX7310_PIN_MODE_INPUT(OPD_FAULT)       |   \
+                            MAX7310_PIN_MODE_OUTPUT(OPD_EN)         |   \
+                            MAX7310_PIN_MODE_OUTPUT(OPD_CB_RESET)   |   \
+                            MAX7310_PIN_MODE_OUTPUT(OPD_BOOT0)      |   \
+                            MAX7310_PIN_MODE_OUTPUT(OPD_PULLUP)     |   \
+                            MAX7310_PIN_MODE_OUTPUT(OPD_LED))
+
+static const I2CConfig i2cconfig = {
+    OPMODE_I2C,
+    400000,
+    STD_DUTY_CYCLE,
+};
+
+static const MAX7310Config devconfig = {
+    &I2CD1,
+    &i2cconfig,
+    OPD_PROTOCARD1,
+    OPD_ODR_VAL,
+    OPD_POL_VAL,
+    OPD_MODE_VAL,
+    MAX7310_TIMEOUT_ENABLED
+};
---
+++ src/f4/app_nmt/source/opd.h
@@ -0,0 +1,10 @@
+#ifndef OPD_H
+#define OPD_H
+
+typedef enum {
+    OPD_PROTOCARD1 = 0x18,
+    OPD_PROTOCARD2 = 0x19,
+    OPD_PROTOCARD3 = 0x1A,
+} opd_addr_t;
+
+#endif
