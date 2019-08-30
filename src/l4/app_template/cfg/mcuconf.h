dc04ded566268ae40998c4d87fc0dbf4d46acbf5
Chad Coates <chroco@gmail.com>
2019-08-29 21:06:56 -0700

    Edit cfgs so I2C changes compile

--- src/l4/app_template/cfg/mcuconf.h
+++ src/l4/app_template/cfg/mcuconf.h
@@ -154,7 +154,7 @@
 /*
  * I2C driver system settings.
  */
-#define STM32_I2C_USE_I2C1                  FALSE
+#define STM32_I2C_USE_I2C1                  TRUE
 #define STM32_I2C_USE_I2C2                  FALSE
 #define STM32_I2C_USE_I2C3                  FALSE
 #define STM32_I2C_USE_I2C4                  FALSE
