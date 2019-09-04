38aa6f8b2af8b30cd88d9859bb693f46cdf6f3ba
Miles Simpson <miles.a.simpson@gmail.com>
2019-09-03 17:41:02 -0700

    Attempting to optimize CANopen subsystem

--- common/oresat.c
+++ common/oresat.c
@@ -10,52 +10,6 @@ typedef struct {
 static worker_t workers[ORESAT_MAX_THREADS];
 static uint32_t num_workers;
 
-static thread_t *can_rt_tp, *can_events_tp;
-
-/* CAN Worker Threads */
-THD_WORKING_AREA(can_rt_wa, 0x64);
-THD_FUNCTION(can_rt, p)
-{
-    systime_t prev_time, cur_time, diff_time;
-    CO_t *CO = p;
-
-    // Set thread name
-    chRegSetThreadName("can_rt");
-
-    prev_time = chVTGetSystemTimeX();
-    // Start Loop
-    while (!chThdShouldTerminateX()) {
-        if (CO->CANmodule[0]->CANnormal) {
-            bool_t syncWas;
-
-            /* Process Sync and read inputs */
-            diff_time = chTimeDiffX(prev_time, cur_time = chVTGetSystemTimeX());
-            syncWas = CO_process_SYNC_RPDO(CO, chTimeI2US(diff_time));
-            /* Further I/O or nonblocking application code may go here. */
-            /* Write outputs */
-            diff_time = chTimeDiffX(prev_time, cur_time = chVTGetSystemTimeX());
-            CO_process_TPDO(CO, syncWas, chTimeI2US(diff_time));
-            prev_time = cur_time;
-        }
-
-        chThdSleepMicroseconds(1000);
-    }
-
-    chThdExit(MSG_OK);
-}
-
-void CO_NMT_cb(CO_NMT_internalState_t state)
-{
-    syssts_t sts;
-    sts = chSysGetStatusAndLockX();
-    if (state == CO_NMT_OPERATIONAL) {
-        chEvtSignalI(can_events_tp, EVENT_MASK(ORESAT_NMT_OPERATIONAL));
-    } else {
-        chEvtSignalI(can_events_tp, EVENT_MASK(ORESAT_NMT_NONOPERATIONAL));
-    }
-    chSysRestoreStatusX(sts);
-}
-
 void oresat_NMT_event(eventid_t id)
 {
     if (id == ORESAT_NMT_OPERATIONAL) {
@@ -119,8 +73,9 @@ void oresat_init(uint8_t node_id, uint16_t bitrate)
 
 void oresat_start(CANDriver *cand)
 {
+    event_listener_t el;
+    eventmask_t events;
     CO_NMT_reset_cmd_t reset = CO_RESET_NOT;
-    uint16_t sleep_ms;
     systime_t prev_time, cur_time, diff_time;
 
     while (reset != CO_RESET_APP) {
@@ -132,35 +87,29 @@ void oresat_start(CANDriver *cand)
             CO_errorReport(CO->em, CO_EM_MEMORY_ALLOCATION_ERROR, CO_EMC_SOFTWARE_INTERNAL, err);
         }
 
-        reg_event(ORESAT_NMT_OPERATIONAL, oresat_NMT_event);
-        reg_event(ORESAT_NMT_NONOPERATIONAL, oresat_NMT_event);
-
-        can_rt_tp = chThdCreateStatic(can_rt_wa, sizeof(can_rt_wa), HIGHPRIO, can_rt, CO);
-        can_events_tp = chThdCreateStatic(can_events_wa, sizeof(can_events_wa), HIGHPRIO-1, can_events, NULL);
+        chEvtRegister(&CO->CANmodule[0]->rx_event, &el, ALL_EVENTS);
 
         cand->rxfull_cb = CO_CANrx_cb;
         cand->txempty_cb = CO_CANtx_cb;
 
         CO_CANsetNormalMode(CO->CANmodule[0]);
 
-        CO_NMT_initCallback(CO->NMT, CO_NMT_cb);
-
         reset = CO_RESET_NOT;
         prev_time = chVTGetSystemTimeX();
         while (reset == CO_RESET_NOT) {
-            sleep_ms = 50U;
+            events = chEvtWaitAnyTimeout(ALL_EVENTS, TIME_MS2I(OD_producerHeartbeatTime));
+
+            /* Calculate time difference */
             diff_time = chTimeDiffX(prev_time, cur_time = chVTGetSystemTimeX());
             prev_time = cur_time;
-            reset = CO_process(CO, chTimeI2MS(diff_time), &sleep_ms);
-            if (sleep_ms)
-                chThdSleepMilliseconds(sleep_ms);
+
+            /* Process CO objects */
+            CO_process_TPDO(CO, CO_process_SYNC_RPDO(CO, chTimeI2US(diff_time)),
+                    chTimeI2US(diff_time));
+            reset = CO_process(CO, chTimeI2MS(diff_time), NULL);
         }
 
-        chThdTerminate(can_rt_tp);
-        chThdTerminate(can_events_tp);
-        chThdWait(can_rt_tp);
-        chThdWait(can_events_tp);
-        unreg_all_events();
+        chEvtUnregister(&CO->CANmodule[0]->rx_event, &el);
     }
 
     CO_delete((uint32_t)cand);
