543a50b427db3311e8e9d39455bafe58731c7b0f
Miles Simpson <miles.a.simpson@gmail.com>
2019-09-05 08:33:30 -0700

    Updated canable script to check for uid 0

--- canable.sh
+++ canable.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+user=`id -u`
+
+if [ $user -ne 0 ]
+    then
+        echo "Run as root"
+        exit
+fi
+
+if [ $# -eq 0 ]
+    then
+        echo "No argument supplied"
+        exit
+fi
+
 killall -q slcand
 slcand -o -c -f -s8 $1 can0
 sleep 1
