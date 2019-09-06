eee42594aa8c3c0b41a8091406fc178cc4e296e7
Miles Simpson <miles.a.simpson@gmail.com>
2019-09-05 22:13:19 -0700

    Revert "Updated all interval units to be 64 bits"
    This reverts commit bbb546217e8e35a4729ea4492e29d681693acca4.

--- src/f0/app_solar_v3/cfg/chconf.h
+++ src/f0/app_solar_v3/cfg/chconf.h
@@ -60,7 +60,7 @@
  * @note    Allowed values are 16, 32 or 64 bits.
  */
 #if !defined(CH_CFG_INTERVALS_SIZE)
-#define CH_CFG_INTERVALS_SIZE               64
+#define CH_CFG_INTERVALS_SIZE               32
 #endif
 
 /**
