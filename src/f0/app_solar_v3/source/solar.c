1c6bf12aa649240a8e7f4b93aac33c07c4e24499
Miles Simpson <miles.a.simpson@gmail.com>
2019-09-20 10:04:24 -0700

    Added some comments and additional code to solar app, updated for curr_lsb use

--- src/f0/app_solar_v3/source/solar.c
+++ src/f0/app_solar_v3/source/solar.c
@@ -11,17 +11,18 @@ static const I2CConfig i2cconfig = {
     0
 };
 
-static INA226Config ina226config = {
+static const INA226Config ina226config = {
     &I2CD1,
     &i2cconfig,
     INA226_SADDR,
     INA226_CONFIG_MODE_SHUNT_VBUS | INA226_CONFIG_MODE_CONT |
     INA226_CONFIG_VSHCT_1100US | INA226_CONFIG_VBUSCT_1100US |
     INA226_CONFIG_AVG_1,
-    5120        /* 10uA/bit with 0.1mOhm shunt */
+    5120,       /* 10uA/bit with 0.1mOhm shunt */
+    10          /* 10uA/bit */
 };
 
-static MAX580XConfig max580xconfig = {
+static const MAX580XConfig max580xconfig = {
     &I2CD1,
     &i2cconfig,
     MAX5805_SADDR,
@@ -39,15 +40,28 @@ THD_WORKING_AREA(solar_wa, 0x100);
 THD_FUNCTION(solar, arg)
 {
     (void)arg;
+    uint32_t pwr, iadj_v;
 
+    /* Start up drivers for I2C devices */
     ina226Start(&ina226dev, &ina226config);
     max580xStart(&max580xdev, &max580xconfig);
 
     while (!chThdShouldTerminateX()) {
+        /* Get current values for power and iadj voltage */
+        pwr = ina226ReadPower(&ina226dev);
+        iadj_v = max580xReadVoltage(&max580xdev, MAX580X_CODE_LOAD);
+
+        /* Calculate perterbation of iadj */
+
+        /* Write new iadj value */
+        max580xWriteVoltage(&max580xdev, MAX580X_CODE_LOAD, iadj_v);
+
+        /* Toggle LED and sleep */
         palToggleLine(LINE_LED_GREEN);
-        chThdSleepMilliseconds(1000);
+        chThdSleepMilliseconds(100);
     }
 
+    /* Stop drivers for I2C devices */
     max580xStop(&max580xdev);
     ina226Stop(&ina226dev);
 
