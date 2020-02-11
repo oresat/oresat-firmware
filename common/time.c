2271694a0fae6b76f36263012c8cc8a94a39704f
Miles Simpson <miles.a.simpson@gmail.com>
2020-02-11 15:09:55 -0800

    Continuing work on time subsystem

--- common/time.c
+++ common/time.c
@@ -2,18 +2,22 @@
 
 CO_SDO_abortCode_t OD_SCET_Func(CO_ODF_arg_t *ODF_arg)
 {
+    RTCDateTime timespec;
+
     if (ODF_arg->reading) {
-
+        rtcGetTime(&RTCD1, &timespec);
     } else {
-
+        rtcSetTime(&RTCD1, &timespec);
     }
 }
 
 CO_SDO_abortCode_t OD_UTC_Func(CO_ODF_arg_t *ODF_arg)
 {
+    RTCDateTime timespec;
+
     if (ODF_arg->reading) {
-
+        rtcGetTime(&RTCD1, &timespec);
     } else {
-
+        rtcSetTime(&RTCD1, &timespec);
     }
 }
