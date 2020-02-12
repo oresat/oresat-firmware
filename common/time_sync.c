e8cf851edc24c245bfd2d1f129c72627cdbe3a62
Miles Simpson <miles.a.simpson@gmail.com>
2020-02-12 10:28:22 -0800

    Began abstraction of time code

--- common/time_sync.c
+++ common/time_sync.c
@@ -1,13 +1,27 @@
 #include "time.h"
 
+static void get_time_tm(struct tm *timp)
+{
+    RTCDateTime timespec;
+
+    rtcGetTime(&RTCD1, &timespec);
+    rtcConvertDateTimeToStructTm(&timespec, timp, NULL);
+}
+
+static time_t get_time_unix(void)
+{
+    struct tm tim;
+
+    get_time_tm(&tim);
+    return mktime(&tim);
+}
+
 CO_SDO_abortCode_t OD_SCET_Func(CO_ODF_arg_t *ODF_arg)
 {
     RTCDateTime timespec;
 
     if (ODF_arg->reading) {
-        rtcGetTime(&RTCD1, &timespec);
     } else {
-        rtcSetTime(&RTCD1, &timespec);
     }
 }
 
@@ -16,8 +30,6 @@ CO_SDO_abortCode_t OD_UTC_Func(CO_ODF_arg_t *ODF_arg)
     RTCDateTime timespec;
 
     if (ODF_arg->reading) {
-        rtcGetTime(&RTCD1, &timespec);
     } else {
-        rtcSetTime(&RTCD1, &timespec);
     }
 }
