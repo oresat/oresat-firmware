0be37585f852a1f87bb7783e191455338dfeb9dd
Miles Simpson <miles.a.simpson@gmail.com>
2020-02-13 00:18:38 -0800

    Beginning process of driver update

--- common/oresat.mk
+++ common/oresat.mk
@@ -1,27 +1,10 @@
-CANOPEN_SRC   = $(PROJ_SRC)/CANopenNode
-CO_STACK      = $(CANOPEN_SRC)/stack
+include $(PROJ_SRC)/CANopen.mk
 
-PROJINC       = $(PROJ_SRC)/include             \
-                $(CANOPEN_SRC)                  \
-                $(CO_STACK)
+PROJINC       = $(PROJ_SRC)/include
 
-PROJSRC       = $(PROJ_SRC)/CO_driver.c         \
-                $(PROJ_SRC)/CO_master.c         \
-                $(CO_STACK)/CO_SDO.c            \
-                $(CO_STACK)/CO_Emergency.c      \
-                $(CO_STACK)/CO_NMT_Heartbeat.c  \
-                $(CO_STACK)/CO_SYNC.c           \
-                $(CO_STACK)/CO_PDO.c            \
-                $(CO_STACK)/CO_HBconsumer.c     \
-                $(CO_STACK)/CO_SDOmaster.c      \
-                $(CO_STACK)/CO_LSSmaster.c      \
-                $(CO_STACK)/CO_LSSslave.c       \
-                $(CO_STACK)/CO_trace.c          \
-                $(CO_STACK)/crc16-ccitt.c       \
-                $(CANOPEN_SRC)/CANopen.c        \
-                $(PROJ_SRC)/events.c            \
-                $(PROJ_SRC)/sensors.c           \
-                $(PROJ_SRC)/worker.c            \
+PROJSRC       = $(PROJ_SRC)/events.c                    \
+                $(PROJ_SRC)/sensors.c                   \
+                $(PROJ_SRC)/worker.c                    \
                 $(PROJ_SRC)/oresat.c
 
 
