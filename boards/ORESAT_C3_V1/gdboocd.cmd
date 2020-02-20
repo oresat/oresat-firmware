e71492bd0b02e0fd140e135aba8c949ac8d7b639
Miles Simpson <miles.a.simpson@gmail.com>
2020-02-20 14:29:52 -0800

    Fixed C3 board GDB script

--- boards/ORESAT_C3_V1/gdboocd.cmd
+++ boards/ORESAT_C3_V1/gdboocd.cmd
@@ -1,3 +1,3 @@
-target remote | openocd -s ../../../toolchain/openocd -f oocd_ORESAT_C3_V1.cfg -c "gdb_port pipe"
+target remote | openocd -f oocd.cfg -c "gdb_port pipe"
 monitor stm32f4x.cpu configure -rtos ChibiOS
 monitor reset halt
