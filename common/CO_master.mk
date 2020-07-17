2d45dd613cdd648b14c33115cbc8c86fcc4bb504
Miles Simpson <miles.a.simpson@gmail.com>
2020-07-17 10:03:54 -0700

    Added missing source files for CO master

--- common/CO_master.mk
+++ common/CO_master.mk
@@ -4,7 +4,11 @@
 CANOPEN_SRC  = $(PROJ_SRC)/CANopenNode
 
 # List of all CANopen sources for master nodes
-CO_MST_SRC  := $(CANOPEN_SRC)/301/CO_SDOclient.c
+CO_MST_SRC  := $(CANOPEN_SRC)/301/CO_SDOclient.c        \
+               $(CANOPEN_SRC)/301/CO_fifo.c             \
+               $(CANOPEN_SRC)/304/CO_GFC.c              \
+               $(CANOPEN_SRC)/304/CO_SRDO.c             \
+               $(CANOPEN_SRC)/309/CO_gateway_ascii.c
 
 # Required include directories
 CO_MST_INC  := $(CANOPEN_SRC)
