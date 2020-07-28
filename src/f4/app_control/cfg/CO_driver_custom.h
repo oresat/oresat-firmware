d3c4a5231f5dc8d8f92f57deee9b1bb949746dbb
Miles Simpson <miles.a.simpson@gmail.com>
2020-07-28 10:28:43 -0700

    Fixed some slight issues with F4 CO configuration

--- src/f4/app_control/cfg/CO_driver_custom.h
+++ src/f4/app_control/cfg/CO_driver_custom.h
@@ -9,13 +9,21 @@ extern "C" {
 #define CO_NO_GFC 0
 #define CO_NO_SRDO 0
 
+#define CO_CONFIG_SDO_CLI (CO_CONFIG_FLAG_CALLBACK_PRE | \
+                           CO_CONFIG_FLAG_TIMERNEXT | \
+                           CO_CONFIG_SDO_CLI_SEGMENTED | \
+                           CO_CONFIG_SDO_CLI_BLOCK | \
+                           CO_CONFIG_SDO_CLI_LOCAL)
+
 #define CO_CONFIG_GTW (CO_CONFIG_GTW_ASCII | \
                        CO_CONFIG_GTW_ASCII_SDO | \
                        CO_CONFIG_GTW_ASCII_NMT | \
-                       CO_CONFIG_GTW_ASCII_LSS | \
                        CO_CONFIG_GTW_ASCII_LOG | \
                        CO_CONFIG_GTW_ASCII_ERROR_DESC | \
                        CO_CONFIG_GTW_ASCII_PRINT_HELP)
+#define CO_CONFIG_GTW_BLOCK_DL_LOOP 1
+#define CO_CONFIG_GTWA_COMM_BUF_SIZE 2000
+#define CO_CONFIG_GTWA_LOG_BUF_SIZE 2000
 
 #ifdef __cplusplus
 }
