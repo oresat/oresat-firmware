b6adcddf5eb0a06d839053a0558b3b206335f467
Miles Simpson <miles.a.simpson@gmail.com>
2020-08-27 14:09:38 -0700

    Establishing baseline main and shell for radio development and testing

---
+++ src/f4/app_ax5043_driver/source/cmd.c
@@ -0,0 +1,45 @@
+#include "cmd.h"
+#include "chprintf.h"
+#include "shell.h"
+
+static thread_t *shell_tp;
+
+/*===========================================================================*/
+/* OreSat Radio Control                                                      */
+/*===========================================================================*/
+void cmd_radio(BaseSequentialStream *chp, int argc, char *argv[])
+{
+}
+
+/*===========================================================================*/
+/* Shell                                                                     */
+/*===========================================================================*/
+static const ShellCommand commands[] = {
+    {"radio", cmd_radio},
+    {NULL, NULL}
+};
+
+static char histbuf[SHELL_MAX_HIST_BUFF];
+
+static const ShellConfig shell_cfg = {
+    (BaseSequentialStream *)&SD3,
+    commands,
+    histbuf,
+    sizeof(histbuf),
+};
+
+THD_WORKING_AREA(shell_wa, 0x1000);
+THD_WORKING_AREA(cmd_wa, 0x200);
+THD_FUNCTION(cmd, arg)
+{
+    (void)arg;
+
+    /* Start a shell */
+    while (!chThdShouldTerminateX()) {
+        shell_tp = chThdCreateStatic(shell_wa, sizeof(shell_wa), NORMALPRIO, shellThread, (void *)&shell_cfg);
+        chThdWait(shell_tp);
+        chThdSleepMilliseconds(500);
+    }
+
+    chThdExit(MSG_OK);
+}
