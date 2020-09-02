34bf347a7591ba5e8886a46fa8ff1bc244b86c7a
Miles Simpson <miles.a.simpson@gmail.com>
2020-09-02 13:59:39 -0700

    Initialized some basic testing code for BMI088 IMU

--- src/f0/app_imu/source/imu.c
+++ src/f0/app_imu/source/imu.c
@@ -4,7 +4,7 @@
 /* TODO: Set this to the correct slave address for the BMI088 device */
 #define BMI088_SADDR 0x00U
 
-static const I2CConfig i2cconfig = {
+static const I2CConfig i2ccfg = {
     STM32_TIMINGR_PRESC(0xBU) |
     STM32_TIMINGR_SCLDEL(0x4U) | STM32_TIMINGR_SDADEL(0x2U) |
     STM32_TIMINGR_SCLH(0xFU)  | STM32_TIMINGR_SCLL(0x13U),
@@ -13,9 +13,9 @@ static const I2CConfig i2cconfig = {
 };
 
 static const BMI088Config imucfg = {
-    &I2CD1,
-    &i2ccfg,
-    BMI088_SADDR,
+    .i2cp = &I2CD1,
+    .i2ccfg = &i2ccfg,
+    .saddr = BMI088_SADDR,
 };
 
 static BMI088Driver imudev;
