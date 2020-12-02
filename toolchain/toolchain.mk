1c950c7e48aadc373b2590a35b4e5345aa831540
Miles Simpson <miles.a.simpson@gmail.com>
2020-12-02 13:30:40 -0800

    'make write' now uses OpenOCD, and the programmer serial can be specified by SERIAL parameter

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -4,18 +4,18 @@ OOCD_CFG = oocd.cfg
 GDB_OOCD_CFG = gdboocd.cmd
 GDB_STL_CFG = gdbstl.cmd
 
-write: $(APP_HEXFILE) write_stl
+write: $(APP_HEXFILE) write_ocd
 
 write_ocd:
-	openocd -s $(BOARDDIR) -f $(OOCD_CFG) -c "program $(APP_HEXFILE) verify reset exit"
+	openocd -s $(BOARDDIR) -f $(OOCD_CFG) -c "hla_serial $(SERIAL); program $(APP_HEXFILE) verify reset exit"
 
 write_stl:
-	st-flash $(ARGS) --reset --format ihex write $(APP_HEXFILE)
+	st-flash --reset --format ihex write $(APP_HEXFILE)
 
 gdb: $(GDB_ELF) gdb_ocd
 
 gdb_ocd:
-	$(TRGT)gdb -q $(shell pwd)/$(GDB_ELF) -cd $(BOARDDIR) -x $(GDB_OOCD_CFG)
+	$(TRGT)gdb -q $(shell pwd)/$(GDB_ELF) -cd $(BOARDDIR) -ex "target remote | openocd -f oocd.cfg -c 'hla_serial $(SERIAL); gdb_port pipe'" -x $(GDB_OOCD_CFG)
 
 gdb_stl:
 	$(TRGT)gdb -q $(shell pwd)/$(GDB_ELF) -cd $(TOOLCHAIN) -x ./$(GDB_STL_CFG)
