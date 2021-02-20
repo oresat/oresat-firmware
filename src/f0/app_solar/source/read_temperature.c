5f4bed11869555fb5402a1e6bbb9ad23c49c1a54
Ted Havelka <ted@cs.pdx.edu>
2021-02-19 22:46:49 -0800

    Adding temperature averaging, development routine to report stats via UART.

--- src/f0/app_solar/source/read_temperature.c
+++ src/f0/app_solar/source/read_temperature.c
@@ -22,8 +22,12 @@
 
 #define SOLAR_BOARD_TEMPERATURE_READINGS_FREQ_IN_MS (4000)
 #define DELAY_BETWEEN_TEMP_READINGS_IN_MS (50)
-#define TEMPERATURE_READING_SAMPLE_SIZE_TO_AVERAGE (10)
+
 #define SIZE_OF_TMP101AN_READING_REGISTER_IN_BYTES (2)
+
+#define TEMPERATURE_READING_SAMPLE_SIZE_TO_AVERAGE (10)
+#define MULTIPLIER_TO_RETAIN_TENTHS (10)
+
 
 
 //----------------------------------------------------------------------
@@ -76,6 +80,9 @@ static uint16_t reading_sensor_01_max = 0;
 
 static uint16_t reading_sensor_02_min = 0;
 static uint16_t reading_sensor_02_max = 0;
+
+static uint32_t reading_sensor_01_average = 0;
+static uint32_t reading_sensor_02_average = 0;
 
 
 
@@ -154,7 +161,78 @@ bool update_min_max_temperature(uint8_t sensor_addr, uint16_t latest_reading)
 
     return routine_status;
 
-} // end routine store_reading()
+} // end update_min_max_temperature()
+
+
+
+bool update_average_temperature(uint8_t sensor_addr)
+{
+    bool routine_status = false;
+    uint16_t i = 0;
+    uint32_t sum_of_readings = 0;
+//static uint16_t readings_sensor_01_count = 0;
+//static uint16_t readings_sensor_02_count = 0;
+
+    switch (sensor_addr)
+    {
+        case I2C_ADDR_SENSOR_01:
+            if ( readings_sensor_01_count > 0 )
+            {
+                i = readings_sensor_01_count;
+                while ( i > 0 )
+                {
+                    sum_of_readings += readings_sensor_01[(i - 1)];
+                }
+                reading_sensor_01_average = ((sum_of_readings * MULTIPLIER_TO_RETAIN_TENTHS) / readings_sensor_01_count);
+                routine_status = true;
+            }
+            break;
+
+        case I2C_ADDR_SENSOR_02:
+            if ( readings_sensor_02_count > 0 )
+            {
+                i = readings_sensor_02_count;
+                while ( i > 0 )
+                {
+                    sum_of_readings += readings_sensor_02[(i - 1)];
+                }
+                reading_sensor_02_average = ((sum_of_readings * MULTIPLIER_TO_RETAIN_TENTHS) / readings_sensor_02_count);
+                routine_status = true;
+            }
+            break;
+
+        default:
+            routine_status = false;  // got invalid temperature sensor I2C address
+    }
+
+    return routine_status;
+
+} // end routine update_average_temperature()
+
+
+
+bool report_temperature_stats(uint8_t sensor_addr)
+{
+    bool routine_status = true;
+
+    switch (sensor_addr)
+    {
+        case I2C_ADDR_SENSOR_01:
+            chprintf((BaseSequentialStream *) &SD2, "temp sensor 01 min, max, average:  %u, %u, %u\r\n",
+              reading_sensor_01_min, reading_sensor_01_max, reading_sensor_01_average);
+            break;
+
+        case I2C_ADDR_SENSOR_02:
+            chprintf((BaseSequentialStream *) &SD2, "temp sensor 02 min, max, average:  %u, %u, %u\r\n",
+              reading_sensor_02_min, reading_sensor_02_max, reading_sensor_02_average);
+            break;
+
+        default:
+            routine_status = false;
+    }
+    return routine_status;
+}
+
 
 
 //----------------------------------------------------------------------
@@ -215,13 +293,20 @@ THD_FUNCTION(read_temperature, arg)
 
         store_reading(device_driver_for_temp_sensor_01.config->saddr, latest_reading);
         update_min_max_temperature(device_driver_for_temp_sensor_01.config->saddr, latest_reading);
-//        update_average_temperature(device_driver_for_temp_sensor_01.config->saddr);
+        update_average_temperature(device_driver_for_temp_sensor_01.config->saddr);
         chThdSleepMilliseconds(DELAY_BETWEEN_TEMP_READINGS_IN_MS);
 
-
+// Read second temperature sensor, update stats:
         read_tmp101an_temperature_v2(&device_driver_for_temp_sensor_02, byte_array);
+
         latest_reading = ((byte_array[0] << 4) | (byte_array[1] >> 4));
+
         store_reading(device_driver_for_temp_sensor_02.config->saddr, latest_reading);
+        update_min_max_temperature(device_driver_for_temp_sensor_02.config->saddr, latest_reading);
+        update_average_temperature(device_driver_for_temp_sensor_02.config->saddr);
+
+        report_temperature_stats(device_driver_for_temp_sensor_01.config->saddr);
+        report_temperature_stats(device_driver_for_temp_sensor_02.config->saddr);
 
         chThdSleepMilliseconds(SOLAR_BOARD_TEMPERATURE_READINGS_FREQ_IN_MS - DELAY_BETWEEN_TEMP_READINGS_IN_MS);
     }
