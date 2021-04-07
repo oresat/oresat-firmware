7e3495e533751998effc438656280109ee7f38ad
Armaan Roshani <armaan@gmail.com>
2021-04-07 22:34:19 +0300

    enabled TIM1 for PWM

---
+++ src/l4/app_foc/source/app_foc.h
@@ -0,0 +1,11 @@
+#ifndef APP_FOC_H
+#define APP_FOC_H
+
+#include "BLDCMotor.h"
+#include "MagneticSensorSPI.h"
+#include "drivers/BLDCDriver6PWM.h"
+#include "current_sense/InlineCurrentSense.h"
+#include "communication/Commander.h"
+#include "communication/StepDirListener.h"
+
+#endif
