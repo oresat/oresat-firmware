ed8c2bb19ffc606cdc4ce6643357e59c3a176bfb
Miles Simpson <miles.a.simpson@gmail.com>
2021-04-17 10:13:38 -0700

    Fix FIFO1 CAN filter implementation

--- common/include/CO_threads.h
+++ common/include/CO_threads.h
@@ -3,6 +3,6 @@
 
 extern event_source_t nmt_event;
 
-void CO_init(CANDriver *CANptr, uint8_t node_id, uint16_t bitrate, const CANFilter *fifo1_filters, size_t filter_count);
+void CO_init(CANDriver *CANptr, uint8_t node_id, uint16_t bitrate, const flt_reg_t *fifo1_filters, size_t filter_count);
 void CO_run(CO_t *CO);
 void CO_kill(void);
