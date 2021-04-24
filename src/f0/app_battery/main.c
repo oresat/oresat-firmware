daae015fcc553f3d1de807748c46d233e5d157b9
Miles Simpson <miles.a.simpson@gmail.com>
2021-04-24 10:40:46 -0700

    Update app_battery oresat config

--- src/f0/app_battery/main.c
+++ src/f0/app_battery/main.c
@@ -33,9 +33,9 @@ static thread_descriptor_t battery_worker_desc = {
 };
 
 static oresat_config_t oresat_conf = {
-    &CAND1,
-    0x02,
-    ORESAT_DEFAULT_BITRATE
+    .cand = &CAND1,
+    .node_id = ORESAT_DEFAULT_ID,
+    .bitrate = ORESAT_DEFAULT_BITRATE,
 };
 
 /**
