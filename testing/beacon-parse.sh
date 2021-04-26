70a856ccc8bbe6dfa5b9ec68a47aacd618b30a8e
Oliver Rew <oliver.rew@rigado.com>
2021-04-26 01:17:31 +0100

    decode all the things!

--- testing/beacon-parse.sh
+++ testing/beacon-parse.sh
@@ -26,144 +26,151 @@ def main():
     # open serial
     ser = Serial(tty, 38400, timeout=15.0)
 
+    uint8 = 'B'
+    int8 = 'b'
+    uint16 = 'H'
+    int16 = 'h'
+    uint32 = 'I'
+    int32 = 'i'
+    state = 'c' # state represented as char
+
     parts = [
-        {'name': 'APRS Data Type', 't': 'B'}, # uint8
-        {'name': 'APRS Revision', 't': 'B'}, # uint8
-        {'name': 'Oresat0 State', 't': 'c', 'fmt': lambda s : str(s)}, # char # TODO might not need for char
-        {'name': 'Uptime', 't': 'I'}, # uint32
-        {'name': 'Time', 't': 'I'}, # uint64
-        {'name': 'Temperature', 't': 'b'}, # int8
-        {'name': 'Ref Voltage', 't': 'B'}, # uint8
-        {'name': 'Vbusp Voltage', 't': 'B'}, # uint8
-        {'name': 'Vbusp Current', 't': 'B'}, # uint8
-        #{'name': 'Num Timeouts', 't': 'H'}, # uint16
-        #{'name': 'eMMC % Full', 't': 'B'}, # uint8
-        #{'name': 'L Band RX Bytes', 't': '??'},
-        #{'name': 'L Band RX Valid Packets', 't': '??'},
-        #{'name': 'L Band RX RSSI', 't': 'B'}, # uint8
-        #{'name': 'L Band RX PLL Lock', 't': 'c'}, # state
-        #{'name': 'UHF TX Temperature', 't': 'b'}, # int8
-        #{'name': 'UHF TX FWD PWR', 't': 'H'}, # uint16
-        #{'name': 'UHF TX REV PWE', 't': 'H'}, # uint16
-        #{'name': 'UHF RX Bytes', 't': '??'},
-        #{'name': 'UHF RX Valid Packets', 't': '??'},
-        #{'name': 'UHF RX RSSI', 't': 'B'}, # uint8
-        #{'name': 'UHF RX PLL Lock', 't': 'c'}, # state
+        {'name': 'APRS Data Type', 't': uint8}, 
+        {'name': 'APRS Revision', 't': uint8}, 
+        {'name': 'Oresat0 State', 't': state, 'fmt': lambda s : str(s)}, # TODO might not need for char
+        {'name': 'Uptime', 't': uint32}, 
+        {'name': 'Time', 't': uint32}, 
+        {'name': 'Temperature', 't': int8}, 
+        {'name': 'Ref Voltage', 't': uint8}, 
+        {'name': 'Vbusp Voltage', 't': uint8},
+        {'name': 'Vbusp Current', 't': uint8},
+        {'name': 'Num Timeouts', 't': uint16}, 
+        {'name': 'eMMC % Full', 't': uint8}, 
+        {'name': 'L Band RX Bytes', 't': uint32},
+        {'name': 'L Band RX Valid Packets', 't': uint32},
+        {'name': 'L Band RX RSSI', 't': uint8}, 
+        {'name': 'L Band RX PLL Lock', 't': state},
+        {'name': 'UHF TX Temperature', 't': int8},
+        {'name': 'UHF TX FWD PWR', 't': uint16},
+        {'name': 'UHF TX REV PWE', 't': uint16},
+        {'name': 'UHF RX Bytes', 't': uint32},
+        {'name': 'UHF RX Valid Packets', 't': uint32},
+        {'name': 'UHF RX RSSI', 't': uint8},
+        {'name': 'UHF RX PLL Lock', 't': state},
         #{'name': 'Deployer 1', 't': '??'},
-        #{'name': 'CAN1', 't': 'c'}, # state
-        #{'name': 'CAN2', 't': 'c'}, # state
-        #{'name': 'OPD Current', 't': 'B'}, # uint8
-        #{'name': 'OPD State', 't': 'c'}, # state
+        #{'name': 'CAN1', 't': state}, 
+        #{'name': 'CAN2', 't': state}, 
+        #{'name': 'OPD Current', 't': uint8}, 
+        #{'name': 'OPD State', 't': state}, 
 
-        #{'name': 'Battery Pack 1 Vbatt', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 1 Vcell', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 1 Vcell max', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 1 Vcell min', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 1 Vcell 1', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 1 Vcell 2', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 1 Vcell avg', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 1 temp', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 1 temp avg', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 1 temp max', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 1 temp min', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 1 current', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 1 current avg', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 1 current max', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 1 current min', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 1 state', 't': 'B'}, # uint8
-        #{'name': 'Battery Pack 1 reported state of change', 't': 'B'}, # uint8 
-        #{'name': 'Battery Pack 1 full capacity', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 1 reported capacity', 't': 'H'}, # uint16
+        #{'name': 'Battery Pack 1 Vbatt', 't': uint16}, 
+        #{'name': 'Battery Pack 1 Vcell', 't': uint16}, 
+        #{'name': 'Battery Pack 1 Vcell max', 't': uint16},
+        #{'name': 'Battery Pack 1 Vcell min', 't': uint16}, 
+        #{'name': 'Battery Pack 1 Vcell 1', 't': uint16}, 
+        #{'name': 'Battery Pack 1 Vcell 2', 't': uint16}, 
+        #{'name': 'Battery Pack 1 Vcell avg', 't': uint16}, 
+        #{'name': 'Battery Pack 1 temp', 't': int16}, 
+        #{'name': 'Battery Pack 1 temp avg', 't': int16}, 
+        #{'name': 'Battery Pack 1 temp max', 't': int16}, 
+        #{'name': 'Battery Pack 1 temp min', 't': int16}, 
+        #{'name': 'Battery Pack 1 current', 't': int16}, 
+        #{'name': 'Battery Pack 1 current avg', 't': int16},
+        #{'name': 'Battery Pack 1 current max', 't': int16},
+        #{'name': 'Battery Pack 1 current min', 't': int16},
+        #{'name': 'Battery Pack 1 state', 't': uint8}, 
+        #{'name': 'Battery Pack 1 reported state of change', 't': uint8},
+        #{'name': 'Battery Pack 1 full capacity', 't': uint16},
+        #{'name': 'Battery Pack 1 reported capacity', 't': uint16},
 
-        #{'name': 'Battery Pack 2 Vbatt', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 2 Vcell', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 2 Vcell max', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 2 Vcell min', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 2 Vcell 1', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 2 Vcell 2', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 2 Vcell avg', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 2 temp', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 2 temp avg', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 2 temp max', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 2 temp min', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 2 current', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 2 current avg', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 2 current max', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 2 current min', 't': 'h'}, # int16
-        #{'name': 'Battery Pack 2 state', 't': 'B'}, # uint8
-        #{'name': 'Battery Pack 2 reported state of change', 't': 'B'}, # uint8 
-        #{'name': 'Battery Pack 2 full capacity', 't': 'H'}, # uint16
-        #{'name': 'Battery Pack 2 reported capacity', 't': 'H'}, # uint16
+        #{'name': 'Battery Pack 2 Vbatt', 't': uint16},
+        #{'name': 'Battery Pack 2 Vcell', 't': uint16},
+        #{'name': 'Battery Pack 2 Vcell max', 't': uint16},
+        #{'name': 'Battery Pack 2 Vcell min', 't': uint16}, 
+        #{'name': 'Battery Pack 2 Vcell 1', 't': uint16}, 
+        #{'name': 'Battery Pack 2 Vcell 2', 't': uint16}, 
+        #{'name': 'Battery Pack 2 Vcell avg', 't': uint16}, 
+        #{'name': 'Battery Pack 2 temp', 't': int16}, 
+        #{'name': 'Battery Pack 2 temp avg', 't': int16}, 
+        #{'name': 'Battery Pack 2 temp max', 't': int16}, 
+        #{'name': 'Battery Pack 2 temp min', 't': int16}, 
+        #{'name': 'Battery Pack 2 current', 't': int16}, 
+        #{'name': 'Battery Pack 2 current avg', 't': int16}, 
+        #{'name': 'Battery Pack 2 current max', 't': int16}, 
+        #{'name': 'Battery Pack 2 current min', 't': int16}, 
+        #{'name': 'Battery Pack 2 state', 't': uint8}, 
+        #{'name': 'Battery Pack 2 reported state of change', 't': uint8}, 
+        #{'name': 'Battery Pack 2 full capacity', 't': uint16}, 
+        #{'name': 'Battery Pack 2 reported capacity', 't': uint16}, 
 
-        #{'name': 'Solar -X Voltage Avg', 't': 'H'}, # uint16
-        #{'name': 'Solar -X Current Avg', 't': 'h'}, # int16
-        #{'name': 'Solar -X Power Avg', 't': 'H'}, # uint16
-        #{'name': 'Solar -X Voltage Max', 't': 'H'}, # uint16
-        #{'name': 'Solar -X Current Max', 't': 'h'}, # int16
-        #{'name': 'Solar -X Power Max', 't': 'H'}, # uint16
-        #{'name': 'Solar -X Energy', 't': 'H'}, # uint16
+        #{'name': 'Solar -X Voltage Avg', 't': uint16}, 
+        #{'name': 'Solar -X Current Avg', 't': int16}, 
+        #{'name': 'Solar -X Power Avg', 't': uint16}, 
+        #{'name': 'Solar -X Voltage Max', 't': uint16}, 
+        #{'name': 'Solar -X Current Max', 't': int16}, 
+        #{'name': 'Solar -X Power Max', 't': uint16}, 
+        #{'name': 'Solar -X Energy', 't': uint16}, 
 
-        #{'name': 'Solar -Y Voltage Avg', 't': 'H'}, # uint16
-        #{'name': 'Solar -Y Current Avg', 't': 'h'}, # int16
-        #{'name': 'Solar -Y Power Avg', 't': 'H'}, # uint16
-        #{'name': 'Solar -Y Voltage Max', 't': 'H'}, # uint16
-        #{'name': 'Solar -Y Current Max', 't': 'h'}, # int16
-        #{'name': 'Solar -Y Power Max', 't': 'H'}, # uint16
-        #{'name': 'Solar -Y Energy', 't': 'H'}, # uint16
+        #{'name': 'Solar -Y Voltage Avg', 't': uint16}, 
+        #{'name': 'Solar -Y Current Avg', 't': int16}, 
+        #{'name': 'Solar -Y Power Avg', 't': uint16}, 
+        #{'name': 'Solar -Y Voltage Max', 't': uint16}, 
+        #{'name': 'Solar -Y Current Max', 't': int16}, 
+        #{'name': 'Solar -Y Power Max', 't': uint16}, 
+        #{'name': 'Solar -Y Energy', 't': uint16}, 
 
-        #{'name': 'Solar +X Voltage Avg', 't': 'H'}, # uint16
-        #{'name': 'Solar +X Current Avg', 't': 'h'}, # int16
-        #{'name': 'Solar +X Power Avg', 't': 'H'}, # uint16
-        #{'name': 'Solar +X Voltage Max', 't': 'H'}, # uint16
-        #{'name': 'Solar +X Current Max', 't': 'h'}, # int16
-        #{'name': 'Solar +X Power Max', 't': 'H'}, # uint16
-        #{'name': 'Solar +X Energy', 't': 'H'}, # uint16
+        #{'name': 'Solar +X Voltage Avg', 't': uint16},
+        #{'name': 'Solar +X Current Avg', 't': int16},
+        #{'name': 'Solar +X Power Avg', 't': uint16}, 
+        #{'name': 'Solar +X Voltage Max', 't': uint16}, 
+        #{'name': 'Solar +X Current Max', 't': int16}, 
+        #{'name': 'Solar +X Power Max', 't': uint16}, 
+        #{'name': 'Solar +X Energy', 't': uint16}, 
 
-        #{'name': 'Solar +Y Voltage Avg', 't': 'H'}, # uint16
-        #{'name': 'Solar +Y Current Avg', 't': 'h'}, # int16
-        #{'name': 'Solar +Y Power Avg', 't': 'H'}, # uint16
-        #{'name': 'Solar +Y Voltage Max', 't': 'H'}, # uint16
-        #{'name': 'Solar +Y Current Max', 't': 'h'}, # int16
-        #{'name': 'Solar +Y Power Max', 't': 'H'}, # uint16
-        #{'name': 'Solar +Y Energy', 't': 'H'}, # uint16
+        #{'name': 'Solar +Y Voltage Avg', 't': uint16}, 
+        #{'name': 'Solar +Y Current Avg', 't': int16}, 
+        #{'name': 'Solar +Y Power Avg', 't': uint16}, 
+        #{'name': 'Solar +Y Voltage Max', 't': uint16},
+        #{'name': 'Solar +Y Current Max', 't': int16},
+        #{'name': 'Solar +Y Power Max', 't': uint16},
+        #{'name': 'Solar +Y Energy', 't': uint16},
 
-        #{'name': 'Star Tracker eMMC Capacity', 't': 'B'}, # uint8
-        #{'name': 'Star Tracker Readable Files', 't': 'B'}, # uint8
-        #{'name': 'Star Tracker Updater Status', 't': 'B'}, # uint8
-        #{'name': 'Star Tracker Updates Cached', 't': 'B'}, # uint8
-        #{'name': 'Star Tracker Right Ascension', 't': 'h'}, # int16
-        #{'name': 'Star Tracker Declination', 't': 'h'}, # int16
-        #{'name': 'Star Tracker Roll', 't': 'h'}, # int16
-        #{'name': 'Star Tracker Timestamp Last Packet', 't': 'I'}, # uint32
+        #{'name': 'Star Tracker eMMC Capacity', 't': uint8},
+        #{'name': 'Star Tracker Readable Files', 't': uint8},
+        #{'name': 'Star Tracker Updater Status', 't': uint8},
+        #{'name': 'Star Tracker Updates Cached', 't': uint8},
+        #{'name': 'Star Tracker Right Ascension', 't': int16},
+        #{'name': 'Star Tracker Declination', 't': int16},
+        #{'name': 'Star Tracker Roll', 't': int16},
+        #{'name': 'Star Tracker Timestamp Last Packet', 't': uint32},
 
-        #{'name': 'GPS eMMC Capacity', 't': 'B'}, # uint8
-        #{'name': 'GPS Readable Files', 't': 'B'}, # uint8
-        #{'name': 'GPS Updater Status', 't': 'B'}, # uint8
-        #{'name': 'GPS Updates Cached', 't': 'B'}, # uint8
-        #{'name': 'GPS Status', 't': 'B'}, # uint8
-        #{'name': 'GPS Num Sats Locked', 't': 'B'}, # uint8
-        #{'name': 'GPS X Pos', 't': 'i'}, # int32
-        #{'name': 'GPS Y Pos', 't': 'i'}, # int32
-        #{'name': 'GPS Z Pos', 't': 'i'}, # int32
-        #{'name': 'GPS X Vel', 't': 'i'}, # int32
-        #{'name': 'GPS Y Vel', 't': 'i'}, # int32
-        #{'name': 'GPS Z Vel', 't': 'i'}, # int32
-        #{'name': 'GPS Timestamp Last Packet', 't': 'I'}, # uint32
+        #{'name': 'GPS eMMC Capacity', 't': uint8},
+        #{'name': 'GPS Readable Files', 't': uint8},
+        #{'name': 'GPS Updater Status', 't': uint8}, 
+        #{'name': 'GPS Updates Cached', 't': uint8}, 
+        #{'name': 'GPS Status', 't': uint8}, 
+        #{'name': 'GPS Num Sats Locked', 't': uint8}, 
+        #{'name': 'GPS X Pos', 't': int32}, 
+        #{'name': 'GPS Y Pos', 't': int32}, 
+        #{'name': 'GPS Z Pos', 't': int32},
+        #{'name': 'GPS X Vel', 't': int32},
+        #{'name': 'GPS Y Vel', 't': int32},
+        #{'name': 'GPS Z Vel', 't': int32}, 
+        #{'name': 'GPS Timestamp Last Packet', 't': uint32},
 
-        #{'name': 'ADS Gyro Roll Dot', 't': 'h'}, # int16
-        #{'name': 'ADS Gyro Pitch Dot', 't': 'h'}, # int16
-        #{'name': 'ADS Gyro Yaw Dot', 't': 'h'}, # int16
-        #{'name': 'ADS Gyro IMU temp', 't': 'b'}, # int8
+        #{'name': 'ADS Gyro Roll Dot', 't': int16},
+        #{'name': 'ADS Gyro Pitch Dot', 't': int16},
+        #{'name': 'ADS Gyro Yaw Dot', 't': int16},
+        #{'name': 'ADS Gyro IMU temp', 't': int8},
 
-        #{'name': 'DxWifi eMMC Capacity', 't': 'B'}, # uint8
-        #{'name': 'DxWifi Readable Files', 't': 'B'}, # uint8
-        #{'name': 'DxWifi Updater Status', 't': 'B'}, # uint8
-        #{'name': 'DxWifi Updates Cached', 't': 'B'}, # uint8
-        #{'name': 'DxWifi Transmitting', 't': 'B'}, # bool
+        #{'name': 'DxWifi eMMC Capacity', 't': uint8},
+        #{'name': 'DxWifi Readable Files', 't': uint8},
+        #{'name': 'DxWifi Updater Status', 't': uint8}, 
+        #{'name': 'DxWifi Updates Cached', 't': uint8}, 
+        #{'name': 'DxWifi Transmitting', 't': uint8}, # bool
 
-        #{'name': 'APRS CRC-32', 't': 'I'}, # uint32
-        #{'name': 'APRS Data Type', 't': 'B'}, # uint8
-
+        #{'name': 'APRS CRC-32', 't': uint32},
+        #{'name': 'APRS Data Type', 't': uint8},
     ]
 
     # build the struct unpack string
@@ -173,6 +180,8 @@ def main():
         unpack_str += p['t']
         #if 'fmt' in p.keys():
         #    print(p['fmt'])
+        
+    print("Min Beacon Size: ", struct.calcsize(unpack_str))
 
     while 1:
         try:
@@ -199,12 +208,17 @@ def main():
         #print(data)
         #print(line.hex())
 
-        print("===========================")
-        print("Beacon Len: ", len(line))
+        header = "========= Beacon: " + str(len(line))  + " bytes ========="
+        print(header)
+        print("RAW: ", line.hex())
         for i in range(len(data)):
             p = parts[i]
             print(p['name'] + ": " + str(data[i]))
-        print("===========================")
+        footer = '='
+        for i in range(len(header)):
+            footer += '='
+
+        print(footer)
         print("")
 
 try:
