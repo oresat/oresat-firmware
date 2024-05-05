3391bbe903550fe39d6687b6397a4661f5eaabc8
Chad Coates <chroco@gmail.com>
2024-05-04 17:15:20 -0700

    Create generic app for the nucleo64

---
+++ src/f0/app_nucleo/main.c
@@ -0,0 +1,83 @@
+/*
+    ChibiOS - Copyright (C) 2006..2018 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/* ChibiOS header files */
+#include "ch.h"
+#include "hal.h"
+#include "chprintf.h"
+
+/* Project header files */
+#include "oresat.h"
+#include "nucleo.h"
+
+#define DEBUG_SERIAL    (BaseSequentialStream*) &SD2
+
+static worker_t blink_worker;
+static thread_descriptor_t blink_worker_desc = {
+  .name = "blink count tpdo thread",
+  .wbase = THD_WORKING_AREA_BASE(blink_wa),
+  .wend = THD_WORKING_AREA_END(blink_wa),
+  .prio = NORMALPRIO,
+  .funcp = blink,
+  .arg = NULL
+};
+
+static oresat_config_t oresat_conf = {
+  .cand = &CAND1,
+  //.node_id = ORESAT_DEFAULT_ID,PROTOCARD_NODE_ID
+  .node_id = PROTOCARD_NODE_ID,
+  .bitrate = ORESAT_DEFAULT_BITRATE,
+};
+
+/**
+ * @brief App Initialization
+ */
+static void app_init(void)
+{
+  /* App initialization */
+  reg_worker(&blink_worker, &blink_worker_desc, true, true);
+  
+  /* Start up debug output */
+  sdStart(&SD2, NULL);
+}
+
+/**
+ * @brief Main Application
+ */
+
+
+int main(void)
+{
+  // Initialize and start
+  oresat_init(&oresat_conf);
+  app_init();
+  chprintf(DEBUG_SERIAL, "\r\nStarting app_nucleo...\r\n");
+  oresat_start();
+
+/*
+  halInit();
+  chSysInit();
+
+  while (true)
+  {
+    palClearLine(LINE_LED);
+    chThdSleepMilliseconds(500);
+    palSetLine(LINE_LED);
+    chThdSleepMilliseconds(500);
+  }
+*/
+  return 0;
+}
