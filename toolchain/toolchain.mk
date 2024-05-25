178ab4d60546af20446c1b796a1652e5a3ad274e
Theo Hill <Theo0x48@gmail.com>
2024-05-25 01:59:43 -0700

    Fix srec_cat metadata offset
    While the *.crc32.bin images are correct, converting them to hex with
    srec_cat didn't get the updated metadata offset, so make write (but not
    normal bootloader updates) would write the image in the wrong place.

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -14,7 +14,7 @@ ifeq ($(USE_BOOTLOADER),yes)
 POST_MAKE_ALL_RULE_HOOK:
 	python3 $(TOOLCHAIN)/f0_pack_image.py $(BUILDDIR)/$(PROJECT).bin $(BUILDDIR)/$(PROJECT).crc32.bin $(DATE)
 	@# Convert the crc32 bin file to a hex file that can be written to the STM32F0 with 'make write'
-	srec_cat $(BUILDDIR)/$(PROJECT).crc32.bin -binary -offset 0x0800A000 -o $(BUILDDIR)/$(PROJECT).bin.hex -Intel -Line_Length 44
+	srec_cat $(BUILDDIR)/$(PROJECT).crc32.bin -binary -offset 0x0800A7F4 -o $(BUILDDIR)/$(PROJECT).bin.hex -Intel -Line_Length 44
 	mv $(BUILDDIR)/$(PROJECT).hex $(BUILDDIR)/$(PROJECT).origional.hex
 	cp $(BUILDDIR)/$(PROJECT).bin.hex $(BUILDDIR)/$(PROJECT).hex
 	xxd -i $(BUILDDIR)/$(PROJECT).crc32.bin > $(BUILDDIR)/$(PROJECT).crc32.bin.h
